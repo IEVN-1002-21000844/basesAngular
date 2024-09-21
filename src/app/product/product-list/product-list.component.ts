905ae0daed9ab01701e45df92017d7fa4274e4d6
Fabian Armando Rodriguez Gomez <21000844@alumnos.utleon.edu.mx>
2024-09-21 10:09:27 -0600

    Commit Sabado 21

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { IProductos } from '../productos';
+
 
 @Component({
   selector: 'app-product-list',     /* Nombre del componente */
@@ -7,15 +9,23 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
 
+  imageWidth:number=200;
+  imageMargin:number=2;
+  muestraImg:boolean=true;
+  listFilter:string='';
+
+  muestrasImg():void{
+    this.muestraImg=!this.muestraImg
+  }
   //*ngIf= true o false
   
-  productos:any[]=[
+  productos:IProductos[]=[
     {
       "productoId":1,
       "Modelo":"Sentra",
       "Descripcion":"4 puertas",
       "Precio": 20000,
-      "Year":2023,
+      "Year":"febrero 2 2023",
       "Marca":"NISSAN",
       "Color":"Azul",
       "ImagenUrl":"https://th.bing.com/th/id/OIP.yxqg01EsTpLP1IoF59T4NgHaE6?rs=1&pid=ImgDetMain.jpg"
@@ -25,7 +35,7 @@ export class ProductListComponent {
       "Modelo":"A4",
       "Descripcion":"4 puertas",
       "Precio": 30000,
-      "Year":2022,
+      "Year":"Marzo 23 2022",
       "Marca":"AUDI",
       "Color":"Blanco",
       "ImagenUrl":"https://th.bing.com/th/id/OIP.IkM1K_U6YcMGIjb7iXKobQHaEo?rs=1&pid=ImgDetMain.jpg"
@@ -35,7 +45,7 @@ export class ProductListComponent {
       "Modelo":"Rio",
       "Descripcion":"4 puertas",
       "Precio": 150000,
-      "Year":2020,
+      "Year":"Septiembre 5 2020",
       "Marca":"KIA",
       "Color":"Rojo",
       "ImagenUrl":"https://th.bing.com/th/id/OIP.L9REaQeu5ZIq5vKMpnMg-wHaFj?rs=1&pid=ImgDetMain.jpg"
